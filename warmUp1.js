34b365e4391f54f8dc1f30ea7bd2c69f619237ad
Your Name <you@example.com>
2020-03-13 10:23:09 +0100

    zakaria

--- warmUp1.js
+++ warmUp1.js
@@ -1,6 +1,31 @@
+
 // 1-using + operator combine your partner first and last name .
+
+
 // 2-find if the number 13 is a multiple of 3 or not.
+
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+
+
 // 4-calculate your age in seconds.
 
 // your code is here
+
+
+ 'halima'+'jilani'.
+
+  % , 13/3 !==0, that means 13 isn t multiple of 3.
+
+  13+14+13+15+16+17+19+13+16+15 = 151/10 = 15,1.
+
+  20*365= 7300 days
+  7300*24= 175200 haours
+  175200*60= 10512000 minutes
+  10512000*60= 630720000 seconds.
+
+
+
+
+
+
+   
