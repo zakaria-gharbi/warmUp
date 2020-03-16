4ab3967cd0764bea1a7614a1d87ac2da4d6ea391
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-03-16 21:19:14 +0100

    warmUp 4

--- warmUp3.js
+++ warmUp3.js
@@ -1,13 +1,12 @@
 // 1 - Write functions larger and smaller that each accept two strings as arguments, and return the larger and smaller strings, respectively
 
 // 2 - Write a function called counter that, when invoked, always returns a number that is one more than the previous invocation. For instance:
-// function counting(n) { 
-    TODO: your code here 
-// } 
-// counting(5); // => '1, 2, 3, 4, 5' 
-// counting(1); // => '1' 
+// function counting(n) {
+// TODO: your code here
+// }
+// counting(5); // => '1, 2, 3, 4, 5'
+// counting(1); // => '1'
 // counting(3); // => '1, 2, 3'
-
 
 // 3 -  Meet & Greet: n numbers are coming to a party for numbers. Introduce them each to all the guests as they arrive. You should end up with output looking something like this one for n = 5
 // welcome 1
@@ -18,4 +17,3 @@
 //
 
 // 4- Write a function shortestWord that works like longestWord, but returns the shortest word instead.
-
