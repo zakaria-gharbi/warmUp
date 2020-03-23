0e47aea7be29872390744ebf393e4da84d716048
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-03-23 21:43:57 +0100

    warmUp 10

--- warmUp10.js
+++ warmUp10.js
@@ -1,9 +1,14 @@
 // Style your code and explain it step by step before jumping to code :D
 // Read carefully, focus and test your code :) ... /\ HaPPy HaCkInG /\
 
-//1. Write a JavaScript function to check whether an `input` is an array or not
-// ex isArray([1,2])=>true
-//    isArray(5)=>false
+// **************** git status ****************
+// **************** git add fileName.js ****************
+// **************** git status ****************
+// **************** git commit -m "your message" ****************
+// **************** git status ****************
+// **************** git push origin master ****************
+
+// 1- Using recursion Write a JavaScript program to find the greatest common divisor (gcd) of two positive numbers.
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
@@ -12,19 +17,8 @@
 // **************** git status ****************
 // **************** git push origin master ****************
 
-//2-Write a JavaScript function to get the first n element of an array.
-//ex  first([1,2,3],1)=>[1]
-//    first([1,2,3,4],2)=>[1,2]
-
-// **************** git status ****************
-// **************** git add fileName.js ****************
-// **************** git status ****************
-// **************** git commit -m "your message" ****************
-// **************** git status ****************
-// **************** git push origin master ****************
-
-//3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
-//ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+// 2-Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
+//you can only add one at each summetion, you'll need to use recursion in this.
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
