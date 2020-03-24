2d5d0975d65a25baa499b15592bc2a7a0fd3c3d6
zakaria.gharbi <hagerbenghdir@gmail.com>
2020-03-24 10:12:27 +0100

    zico

--- warmUp9.js
+++ warmUp9.js
@@ -4,7 +4,13 @@
 //1. Write a JavaScript function to check whether an `input` is an array or not
 // ex isArray([1,2])=>true
 //    isArray(5)=>false
+function check(n){
+	if(n===[]){
+		return "true";
+	}
+        return"false";
 
+}
 // **************** git status ****************
 // **************** git add fileName.js ****************
 // **************** git status ****************
@@ -15,6 +21,19 @@
 //2-Write a JavaScript function to get the first n element of an array.
 //ex  first([1,2,3],1)=>[1]
 //    first([1,2,3,4]2)=>[1,2]
+function theFirst([]){
+  var first =(array,n);
+  if (array===0){
+  	return 0;
+  }
+    else if (n===0){
+    	return array[0];
+    }
+
+
+}
+
+
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
@@ -25,6 +44,16 @@
 
 //3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
 //ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+function convertPairs(arr){
+	var keys =keys(n);
+	var length=keys.length;
+	var pairs=array;
+	for (var i=0;i<arr.length;i++){
+		pairs[i]=[keys[i],arr.keys[i]]:
+	}
+          return pairs; 
+    }
+    
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
