aeafcae099c109352537fd354383f16c4f73e1b7
Houda Rouaissi <35735199+HoudaRs@users.noreply.github.com>
2020-03-25 09:15:52 +0100

    Update warmUp7.js

--- warmUp7.js
+++ warmUp7.js
@@ -7,28 +7,7 @@
 //ex: helloWorld('fr') ==> 'Bonjour tout le monde'
 //    helloWorld('es') ==> 'Hola, Mundo'
 //    helloWorld('') ==> 'Hello, World'
-var helloWorld = lang => {
-  var lang = lang.toLowerCase();
 
-  return lang === ""
-    ? "Hello, World"
-    : lang === "fr"
-    ? "Bonjour tout le monde"
-    : lang === "es"
-    ? "Hola, Mundo"
-    : lang === "ar"
-    ? "صباح الخير"
-    : null;
-};
 
 //3- write a function to Reverse an array without use array.reverse() method:
 // ex: reverseArray([1,2,3]) ==> [3,2,1]
-var reverse = arr => {
-  var result = [];
-
-  for (var i = arr.length - 1; i >= 0; i--) {
-    result.push(arr[i]);
-  }
-
-  return result;
-};
