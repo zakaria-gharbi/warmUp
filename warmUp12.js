c1a9cacd244d24605256ab7faba8370cc09515a7
zakaria.gharbi <hagerbenghdir@gmail.com>
2020-03-26 10:10:00 +0100

    zak

--- warmUp12.js
+++ warmUp12.js
@@ -1,6 +1,22 @@
 // 1-Write a function that takes a string as an input and returns the reverse of each letter
 //followed by a number starting from zero(solve it using while loop).
 // example:    reverseStr('hello'); "o0l1l2e3h4"
+function reverse(str,number){
+var i =0;
+while(number===str.length){
 
+}
+
+
+
+
+}
 // 2-write a function that takes array of strings and returns an array of the strings that have the same length
 // example :  ['hi','hello','welcome','hy'] == > ["hi","hy"]
+ function sameLength(arr[]){
+    var newArry=[];
+    var i=0;
+ 	if (arr.length(i)=== i++){
+ 		return [ "arr.length(i)","i++"] ;
+ 	}
+ }
